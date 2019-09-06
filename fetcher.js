7ddc96ee5c5fe333014386078f8f7ebf826dfb4a
Zongxi Li <zongxiliwork@gmail.com>
2019-09-06 16:36:37 +0000

    new commiot

---
+++ fetcher.js
@@ -0,0 +1,11 @@
+// Get the targetted URL address and Dest File
+outputArr = process.argv.slice(2);
+const urlAddress = outputArr[0];
+const destFile = outputArr[1];
+
+const request = require('request');
+request('http://www.example.com/', (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
